87f05f29d3102e2ccac11c7d6813ac45a7ae8e02
brandon <brandon@gmail.com>
2022-03-27 19:58:15 +0700

    +

---
+++ cg.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+git checkout main
+git pull origin main
+git submodule init
+git submodule update
+
+PROJECT="jobTask"
+GitReversion=`git rev-parse HEAD`
+BuildTime=`date +'%Y.%m.%d.%H%M%S'`
+BuildGoVersion=`go version`
+
+go build -ldflags "-X main.gitReversion=${GitReversion}  -X 'main.buildTime=${BuildTime}' -X 'main.buildGoVersion=${BuildGoVersion}'" -o $PROJECT
+# cg
+sshpass -p $1 scp $PROJECT testp3@34.92.240.177:/home/centos/cg/task/
+
