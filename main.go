3bd4a263821cf0a5c99275eed8489dda8ed39baf
brandon <brandon@ym.com>
2022-04-19 16:56:07 +0700

    fix

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"task/modules/bonus"
 	"task/modules/dividend"
 	"task/modules/evo"
+	"task/modules/message"
 	"task/modules/promo"
 	"task/modules/risk"
 	"task/party"
@@ -30,11 +31,12 @@ var cbP = map[string]fnP{
 type fn func([]string, string)
 
 var cb = map[string]fn{
-	"banner": banner.Parse, //活动流水更新
-	"bonus":  bonus.Parse,  // 电子游戏奖金池
-	"bet":    bet.Parse,    //投注
-	"risk":   risk.Parse,   //风控自动派单脚本
-	"evo":    evo.Parse,    //evo
+	"banner":  banner.Parse,  //活动流水更新
+	"bonus":   bonus.Parse,   // 电子游戏奖金池
+	"bet":     bet.Parse,     //投注
+	"risk":    risk.Parse,    //风控自动派单脚本
+	"evo":     evo.Parse,     //evo
+	"message": message.Parse, //站内信批量发送
 }
 
 func main() {
