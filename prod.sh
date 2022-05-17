65ba260c562bb3b24e7c07ae7fdd59cc0412182a
bran <bran@git.com>
2022-05-17 08:58:28 -0700

    +

---
+++ prod.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+git checkout main
+git pull origin main
+git submodule init
+git submodule update
+
+PROJECT="task"
+GitReversion=`git rev-parse HEAD`
+BuildTime=`date +'%Y.%m.%d.%H%M%S'`
+BuildGoVersion=`go version`
+
+go build -ldflags "-X main.gitReversion=${GitReversion}  -X 'main.buildTime=${BuildTime}' -X 'main.buildGoVersion=${BuildGoVersion}'" -o $PROJECT
+mv $PROJECT /opt/deploy/cg/$PROJECT
+cd /opt/deploy/cg/$PROJECT
+
+git pull
+git commit -am "${GitReversion}"
+git push
+
