821ee2b495659d1a01a22291e129dd9e2dbb99af
brandon <brandon@gmail.com>
2022-05-30 19:35:55 +0700

    +短信过期脚本

---
+++ modules/sms/main.go
@@ -0,0 +1,61 @@
+package sms
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/panjf2000/ants/v2"
+	cpool "github.com/silenceper/pool"
+	"task/contrib/conn"
+	"task/modules/common"
+	"time"
+)
+
+var (
+	td       *sqlx.DB
+	beanPool cpool.Pool
+	prefix   string
+)
+
+func Parse(endpoints []string, path string) {
+
+	conf := common.ConfParse(endpoints, path)
+	prefix = conf.Prefix
+
+	// 初始化beanstalk
+	beanPool = conn.InitBeanstalk(conf.Beanstalkd.Addr, 50, 50, 100)
+	// 初始化td
+	td = conn.InitTD(conf.Td.Addr, conf.Td.MaxIdleConn, conf.Td.MaxOpenConn)
+	common.InitTD(td)
+
+	tdTask()
+}
+
+// 批量红利派发
+func tdTask() {
+
+	common.Log("sms", "短信自动过期脚本开始")
+
+	// 初始化红利批量发放任务队列协程池
+	promoPool, _ := ants.NewPoolWithFunc(10, func(payload interface{}) {
+
+		if fn, ok := payload.(common.BeansFnParam); ok {
+			tdHandle(fn.M)
+			// 删除job
+			_ = fn.Conn.Delete(fn.ID)
+		}
+	})
+
+	topic := fmt.Sprintf("%s_sms", prefix)
+	attr := common.BeansWatcherAttr{
+		TubeName:       topic,
+		ReserveTimeOut: 2 * time.Minute,
+		Pool:           promoPool,
+	}
+
+	common.BeanstalkWatcher(beanPool, attr)
+}
+
+//短信自动过期
+func tdHandle(m map[string]interface{}) {
+	fmt.Printf("bean data %#v", m)
+}
