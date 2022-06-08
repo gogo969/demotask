e4eeb83ef97446bd8656c9a4bed557d05b5acc5b
brandon <brandon@gmail.com>
2022-06-08 17:11:59 +0700

    fix

--- modules/message/main.go
+++ modules/message/main.go
@@ -109,7 +109,7 @@ func deleteHandle(param map[string]interface{}) {
 	var records []g.Record
 	for _, v := range tss {
 		// 2022-06-07T16:28:26.285+07:00
-		t, _ := time.ParseInLocation("2006-01-02T15:04:05.999999 07:00", v, loc)
+		t, _ := time.ParseInLocation("2006-01-02T15:04:05.999999+07:00", v, loc)
 		record := g.Record{
 			"ts":        t.UnixMicro(),
 			"is_delete": 1,
